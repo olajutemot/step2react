007b23ef4d11a9510a3eb83fe4540e664d021e27
Olajutemo Timilehin <olajutemot@gmail.com>
2023-12-20 12:01:53 -0800

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const messages = [
 ];
 function App() {
   const [step, setSteps] = useState(1);
+  const [close, setClose] = useState(true);
   const handlePrev = () => {
     if (step > 1) {
       setSteps(step - 1);
@@ -17,20 +18,30 @@ function App() {
       setSteps(step + 1);
     }
   };
+  const handleClose = () => {
+    setClose((current) => !current);
+  };
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step{step} : {messages[step - 1]}{" "}
-      </p>
-      <div className="buttons">
-        <button onClick={handlePrev}>Previous</button>
-        <button onClick={handleNext}>Next</button>
-      </div>
+    <div>
+      <button className="close" onClick={handleClose}>
+        &times
+      </button>
+      {close && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step{step} : {messages[step - 1]}{" "}
+          </p>
+          <div className="buttons">
+            <button onClick={handlePrev}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
