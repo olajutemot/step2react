f54438763b74c8216a3b159b146e0c23db0da31d
Olajutemo Timilehin <olajutemot@gmail.com>
2023-12-20 06:28:01 -0800

    first Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 function App() {
+  const [step, setSteps] = useState(1);
+  const handlePrev = () => {
+    if (step > 1) {
+      setSteps(step - 1);
+    }
+  };
+  const handleNext = () => {
+    if (step < 3) {
+      setSteps(step + 1);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        Step{step} : {messages[step - 1]}{" "}
+      </p>
+      <div className="buttons">
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
     </div>
   );
 }
